d72eb4d825f26028a78315365afad1d7d29ca68e
Murilo Martins <mjnmartins1989@gmail.com>
2022-03-12 01:29:11 -0300

    volta commit Terceira questao

--- QuestionThree/src/questionthree/StudFarms.java
+++ QuestionThree/src/questionthree/StudFarms.java
@@ -2,6 +2,28 @@
 package questionthree;
 
 public class StudFarms {
-    int horse, horseshoe;
+   private int horse, horseshoe;
+
+    public StudFarms(int horse, int horseshoe) {
+        this.horse = horse;
+        this.horseshoe = horseshoe;
+    }
+
+    public int getHorse() {
+        return horse;
+    }
+
+    public void setHorse(int horse) {
+        this.horse = horse;
+    }
+
+    public int getHorseshoe() {
+        return horseshoe;
+    }
+
+    public void setHorseshoe(int horseshoe) {
+        this.horseshoe = horseshoe;
+    }
+   
     
 }
