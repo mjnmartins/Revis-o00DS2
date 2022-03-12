55f237e3b5c0e07c562d5a5f26931d894191cb69
Murilo Martins <mjnmartins1989@gmail.com>
2022-03-12 08:35:51 -0300

    primeiro commit Terceira questao

--- QuestionThree/src/questionthree/QuestionThree.java
+++ QuestionThree/src/questionthree/QuestionThree.java
@@ -1,16 +1,12 @@
+
 package questionthree;
 
-import java.util.Scanner;
 
 public class QuestionThree {
 
+   
     public static void main(String[] args) {
-        Scanner horse = new Scanner(System.in);
-        StudFarms h1 = new StudFarms(00,4);
-        System.out.println("Digite a quantidade de cavalos que foram comprados pelo seu haras:");
-        h1.horse = horse.nextInt();
-        h1.horseshoe = (h1.horse * 4);
         
-
     }
+    
 }
