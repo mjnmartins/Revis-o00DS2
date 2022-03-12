47e3ab4e2d4c0b09b80c30317531ccf4a758745f
Murilo Martins <mjnmartins1989@gmail.com>
2022-03-12 19:47:37 -0300

    Primeiro commit questão 1 DS2

--- QuestionOne/src/questionone/QuestionOne.java
+++ QuestionOne/src/questionone/QuestionOne.java
@@ -1,16 +1,19 @@
 
 package questionone;
-
 /**
- Construa uma classe que solicite uma frase escrita pelo usuário.
+ * Construa uma classe que solicite uma frase escrita pelo usuário. 
  * Peça ao usuário para escolher uma palavra da frase escrita e substituí-la por outra palavra.
  * (Utilize o método replace()).
+ */
+/**
+ *
+ * @author Murilo Martins
  */
 public class QuestionOne {
 
     
     public static void main(String[] args) {
-        
+       
     }
     
 }
