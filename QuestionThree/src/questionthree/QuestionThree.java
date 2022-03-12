e4beafe0246f4824fc8faeb7b7c47de356e49097
Murilo Martins <mjnmartins1989@gmail.com>
2022-03-11 21:21:24 -0300

    quarto commit Terceira questao

--- QuestionThree/src/questionthree/QuestionThree.java
+++ QuestionThree/src/questionthree/QuestionThree.java
@@ -1,8 +1,14 @@
 package questionthree;
 
+import java.util.Scanner;
+
 public class QuestionThree {
 
     public static void main(String[] args) {
+        Scanner horse = new Scanner(System.in);
+        StudFarms h1 = new StudFarms();
+        
+        
 
     }
 }
