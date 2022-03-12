8142acb0b995e766d0f0a5c89a051bc7b15190b5
Murilo Martins <mjnmartins1989@gmail.com>
2022-03-11 21:29:46 -0300

    ultimo commit Terceira questao

--- QuestionThree/src/questionthree/QuestionThree.java
+++ QuestionThree/src/questionthree/QuestionThree.java
@@ -7,10 +7,10 @@ public class QuestionThree {
     public static void main(String[] args) {
         Scanner horse = new Scanner(System.in);
         StudFarms h1 = new StudFarms();
-        System.out.println("Digite a quantidade de cavalo que tem no seu haras:");
+        System.out.println("Digite a quantidade de cavalos que foram comprados pelo seu haras:");
         h1.horse = horse.nextInt();
         h1.horseshoe = (h1.horse * 4);
-        System.out.println("O numero de ferraduras para seu haras é:\n" + h1.horseshoe);
+        System.out.println("O numero de ferraduras para os novos cavalos são:\n" + h1.horseshoe);
         
         
         
