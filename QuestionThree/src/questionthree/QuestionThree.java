be35d17a03f38f998563ec17658cb59692fdb982
Murilo Martins <mjnmartins1989@gmail.com>
2022-03-11 21:22:59 -0300

    quinto commit Terceira questao

--- QuestionThree/src/questionthree/QuestionThree.java
+++ QuestionThree/src/questionthree/QuestionThree.java
@@ -7,6 +7,10 @@ public class QuestionThree {
     public static void main(String[] args) {
         Scanner horse = new Scanner(System.in);
         StudFarms h1 = new StudFarms();
+        System.out.println("Digite a quantidade de cavalo que tem no seu haras:");
+        h1.horse = horse.nextInt();
+        h1.horseshoe = (h1.horse * 4);
+        
         
         
 
