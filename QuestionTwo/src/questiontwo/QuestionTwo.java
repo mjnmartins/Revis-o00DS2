e54f8c3da70ae441f48e3fd7692b2d9f2fc18a09
Murilo Martins <mjnmartins1989@gmail.com>
2022-03-12 21:13:31 -0300

    Primeiro commit questão 1 DS2

---
+++ QuestionTwo/src/questiontwo/QuestionTwo.java
@@ -0,0 +1,20 @@
+
+package questiontwo;
+/**
+ * Uma imobiliária vende apenas terrenos retangulares. 
+ * Faça um programa em java para ler as dimensões de um terreno e depois exibir a área do terreno. 
+ * (lado x altura)
+
+ */
+/**
+ *
+ * @author Murilo Martins
+ */
+public class QuestionTwo {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
