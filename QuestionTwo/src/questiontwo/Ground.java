0cdeaf71b95fa2309eb02b2a22841a5c8579bb95
Murilo Martins <mjnmartins1989@gmail.com>
2022-03-13 00:17:37 -0300

    Primeiro commit questão 1 DS2

---
+++ QuestionTwo/src/questiontwo/Ground.java
@@ -0,0 +1,30 @@
+
+package questiontwo;
+
+
+public class Ground {
+    private float length;
+    private float height;
+
+    public Ground() {
+        this.length = 0;
+        this.height = 0;
+    }
+
+    public float getLength() {
+        return length;
+    }
+
+    public void setLength(float length) {
+        this.length = length;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+    
+}
--- QuestionTwo/src/questiontwo/QuestionTwo.java
+++ QuestionTwo/src/questiontwo/QuestionTwo.java
@@ -1,5 +1,10 @@
 
 package questiontwo;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 /**
  * Uma imobiliária vende apenas terrenos retangulares. 
  * Faça um programa em java para ler as dimensões de um terreno e depois exibir a área do terreno. 
@@ -14,6 +19,13 @@ public class QuestionTwo {
 
     
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        Ground g1 = new Ground();
+        System.out.println("Digite o comprimento do terreno:");
+        g1.setLength(keyboard.nextFloat());
+        System.out.println("Digite a altura do terreno:");
+        g1.setH
+        
         
     }
     
